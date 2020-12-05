79f67f759ca554ad28a15a0a98ae32f1d9794e37
Jorge Luis <jdlatc@yahoo.es>
2020-12-05 20:48:37 +0100

    Jorge

--- Readdata.py
+++ Readdata.py
@@ -179,7 +179,7 @@ def ReadWatched(IdUser, ws):
 if __name__ == "__main__":
     Ids = {'Sasha': 1230513, 'Jorge': 1742789, 'Guillermo': 4627260, 'Daniel Gallego': 983049, 'Luminador': 7183467,
     'Will_llermo': 565861}
-    usuario = 'Will_llermo'
+    usuario = 'Jorge'
     print("Se van a importar los datos de ", usuario)
     input("Espero Enter...")
     ExcelName = 'demo_' + usuario + '.xlsx'
