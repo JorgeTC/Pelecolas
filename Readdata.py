1b8958daeff616c96abfd12d9d631b067d3e359e
Jorge Luis <jdlatc@yahoo.es>
2020-12-06 13:39:52 +0100

    Recuperar formatos de numero

--- Readdata.py
+++ Readdata.py
@@ -84,7 +84,15 @@ def PassCaptcha():
 def SetCellValue(ws, line, col, value):
     cell = ws.cell(row = line, column=col)
     cell.value = value
-    # El estilo de la celda ya viene en la plantilla
+    # Configuramos el estilo de la celda
+    if (col == 5): # visionados. Ponemos punto de millar
+        cell.number_format = '#,##0'
+    if (col == 9): # booleano mayor que
+        cell.number_format = '0'
+        cell.font = Font(name = 'SimSun', bold = True)
+        cell.alignment=Alignment(horizontal='center', vertical='center')
+    if (col == 11 or col == 12): #reescala
+        cell.number_format = '0.00'
 
 def PassCaptchaAutomatic(url):
     # no funciona
