5e02bf53c25d3956ec5e92266f3b20cd986fd80b
Jorge Luis <jdlatc@yahoo.es>
2021-08-20 23:12:28 +0200

    Write CSV

--- Otros/Contar palabras.py
+++ Otros/Contar palabras.py
@@ -1,12 +1,40 @@
 import docx
 import nltk
+import stanza
 from nltk.corpus import stopwords
 from nltk import FreqDist
-from nltk.tokenize import wordpunct_tokenize
-from nltk.stem import WordNetLemmatizer
+import datetime
+import sys
 
 PATH="C:\\Users\\usuario\\Desktop\\Jorges things\\Reseñas\\Películas\\"
 NAME="Películas.docx"
+
+class Timer(object):
+    def __init__(self):
+        self.start = datetime.datetime.now()
+
+    def remains(self, done):
+        if done != 0:
+            now  = datetime.datetime.now()
+            left = (1 - done) * (now - self.start) / done
+            sec = int(left.total_seconds())
+            if sec < 60:
+                return "{} seconds   ".format(sec)
+            else:
+                return "{} minutes   ".format(int(sec / 60))
+
+class ProgressBar(object):
+    def __init__(self):
+        self.timer = Timer()
+        self.barLength = 20
+        self.progress = 0.0
+
+    def update(self, done):
+        self.progress = float(done)
+        block = int(round(self.barLength * self.progress))
+        text = "\r[{0}] {1:.2f}% {2}".format( "="*block + " "*(self. barLength-block), self. progress*100, self.timer.remains(self.progress))
+        sys.stdout.write(text)
+        sys.stdout.flush()
 
 def get_full_text(parrfs):
 
@@ -23,7 +51,9 @@ def extract_text():
     file=docx.Document(PATH+NAME)
 
     # Extraigo en una lista todo el texto del documento
-    parr_lis=[parr.text for parr in file.paragraphs if parr.text]
+    parr_lis=[parr.text for parr in file.paragraphs[:10] if parr.text]
+
+    del file
 
     # Extraigo el párrafo en el que he decidido trabajar
     my_parr = get_full_text(parr_lis[1:])
@@ -40,8 +70,26 @@ def list_words(my_parr):
 
     return palabras_punct
 
+
 def desconjugar(palabras):
-    return palabras
+    new_dict = {}
+
+    nlp = stanza.Pipeline("es", verbose=False)
+
+    keys = [palabra[0] for palabra in palabras]
+    total = len(keys)
+    timer = ProgressBar()
+    for index, pal in enumerate(keys):
+        pal_doc = nlp(pal).sentences[0].words[0].lemma
+
+        if pal_doc in new_dict.keys():
+            new_dict[pal_doc] += palabras[index][1]
+        else:
+            new_dict[pal_doc] = palabras[index][1]
+
+        timer.update(index/total)
+
+    return new_dict
 
 def get_frequencies(palabras):
     # Guardamos las frecuencias
@@ -52,13 +100,28 @@ def get_frequencies(palabras):
 
     return freq
 
+def write_csv(freq_dict):
+
+    output_file = open(PATH + "recuento.csv", "w")
+
+    # Escribo los encabezados
+    output_file.write("Palabra,ocurrencias\n")
+
+    for key, val in freq_dict.items():
+        output_file.write("{},{}\n".format(key, val))
+
+    output_file.close()
+
 if __name__ == "__main__":
 
     my_text = extract_text()
 
     palabras = list_words(my_text)
-
+    # Hacemos una primera limpieza de palabras repetidas
     freq = get_frequencies(palabras)
 
-    for key,val in freq:
-        print (str(key) + ':' + str(val))
+    freq = desconjugar(freq)
+
+    write_csv(freq)
+
+    input("Espero enter")
