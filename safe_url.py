f52ab35ec2c6059db0b648e1caa633799b5288cc
Jorge Luis <jdlatc@yahoo.es>
2021-08-27 08:47:59 +0200

    global variable

--- safe_url.py
+++ safe_url.py
@@ -2,6 +2,8 @@ import requests
 import webbrowser
 import time
 
+stopped = False
+
 def safe_get_url(url):
     #open with GET method
     resp = requests.get(url)
@@ -12,12 +14,16 @@ def safe_get_url(url):
         return resp
 
 def PassCaptcha(url):
-    # abro un navegador para poder pasar el Captcha
-    webbrowser.open(url)
+    global stopped
+    if not stopped:
+        stopped = True
+        # abro un navegador para poder pasar el Captcha
+        webbrowser.open(url)
+        print("\nPor favor, entra en FilmAffinity y pasa el captcha por mí.")
     resp = requests.get(url)
-    print("\nPor favor, entra en FilmAffinity y pasa el captcha por mí.")
     # Controlo que se haya pasado el Captcha
     while resp.status_code != 200:
         time.sleep(3) # intento recargar la página cada 3 segundos
         resp = requests.get(url)
+    stopped = False
     return resp
