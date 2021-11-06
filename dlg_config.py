bd31a70b941e4a3536e367a9b976772c6dda3180
Jorge Luis <jdlatc@yahoo.es>
2021-11-06 09:02:07 +0100

    DlgConfig

---
+++ dlg_config.py
@@ -0,0 +1,88 @@
+from .dlg_scroll_base import DlgScrollBase
+import configparser
+
+SZ_FILE = "General.ini"
+
+SZ_SECTIONS = "Secciones: "
+SZ_PARÁMETROS = "Parámetros: "
+SZ_NEW_VALUE = "Nuevo valor de {}: "
+
+S_HTML = "HTML"
+S_READDATA = "READDATA"
+
+P_FILTER_PUBLISHED = "Filter published"
+P_FILTER_FA = "Filter FilmAffinity"
+
+SZ_WELCOME = "## BIENVENIDO A LA CONFIGURACIÓN ##"
+SZ_CLOSE = "### SALIENDO DE LA CONFIGURACIÓN ##"
+
+class DlgConfig(DlgScrollBase):
+
+    def __init__(self):
+        super().__init__(question="", options=[], empty_option=True, empty_ans=True)
+        # Abro el lector del archivo
+        self.config = configparser.ConfigParser()
+        # Abro el archivo
+        self.config.read(SZ_FILE)
+
+        # Qué estoy configurando actualmente
+        self.__curr_section = ""
+        self.__curr_param = ""
+
+        self.fill_default_values()
+
+        print(SZ_WELCOME)
+
+    def __del__(self):
+        with open(SZ_FILE, 'w') as configfile:
+            self.config.write(configfile)
+
+        print(SZ_CLOSE)
+
+    def fill_default_values(self):
+        # Configuraciones para html
+        self.add_default_value(S_HTML, P_FILTER_PUBLISHED, False)
+        # Configuraciones para readdata
+        self.add_default_value(S_READDATA, P_FILTER_FA, 1)
+        pass
+
+    def add_default_value(self, section, param, value):
+        # Si no existe la sección, la añado
+        if section not in self.config:
+            self.config.add_section(section)
+        # Si no existe el parámetro lo añado con el valor default
+        if param not in self.config[section]:
+            self.config.set(section, param, str(value))
+
+    def run(self):
+        self.__choose_section()
+
+
+    def __choose_section(self):
+        # Me muevo hasta la sección que sea menester
+        self.sz_question = SZ_SECTIONS
+        self.sz_options = self.config.sections()
+        self.n_options = len(self.sz_options)
+        self.b_empty_option = True
+        self.__curr_section = self.get_ans()
+
+        if self.__curr_section:
+            self.__choose_param()
+
+    def __choose_param(self):
+        # Me muevo hasta la sección que sea menester
+        self.sz_question = SZ_PARÁMETROS
+        self.sz_options = list(dict(self.config.items(self.__curr_section)).keys())
+        self.n_options = len(self.sz_options)
+        self.b_empty_option = True
+        self.__curr_param = self.get_ans()
+
+        if self.__curr_param:
+            self.__set_param()
+            self.__choose_param()
+        else:
+            self.__choose_section()
+
+    def __set_param(self):
+        ans = input(SZ_NEW_VALUE.format(self.__curr_param))
+        self.config[self.__curr_section][self.__curr_param] = ans
