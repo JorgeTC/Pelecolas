bfb8e784b3f7559a078ee4eb4fa39d61889df20e
Jorge Luis <jdlatc@yahoo.es>
2021-11-18 21:15:13 +0100

    Repeat

--- main_make_html.py
+++ main_make_html.py
@@ -5,6 +5,15 @@ def main(path):
     manage_config()
 
     from Make_html import html
+    from dlg_bool import YesNo
+
     Documento = html(path)
-    Documento.write_html()
-    Documento.copy_labels()
+    b_otra = True
+    dlg_otra = YesNo(question="¿Otra reseña?")
+    while b_otra:
+        Documento.write_html()
+        Documento.copy_labels()
+
+        b_otra = dlg_otra.get_ans()
+        if b_otra:
+            Documento.reset()
