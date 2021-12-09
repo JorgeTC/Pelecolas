982cd666243851a5290d119be8816515a7a641b1
Jorge Luis <jdlatc@yahoo.es>
2021-12-09 10:14:50 +0100

    Comment pdf_writer

--- pdf_writer.py
+++ pdf_writer.py
@@ -3,7 +3,6 @@ import os
 import docx2pdf
 from PyPDF2 import PdfFileMerger
 
-from dlg_config import CONFIG
 from word_folder_mgr import WordFolderMgr
 
 
@@ -16,7 +15,11 @@ class PDFWriter(WordFolderMgr):
         # Lista donde guardo todos los pdf que genere
         sz_pdf = []
 
+        # Aún no existen los pdf.
+        # Recorro los docx que existen, los que voy a convertir a pdf.
+        # Genero el nombre que tendrá el pdf resultante de cada uno de ellos
         for file in self.sz_all_docx:
+            # Cojo el nombre del docx y le cambio la extensión
             sz_pdf_name = self.word_folder / (file.stem + ".pdf")
             sz_pdf.append(sz_pdf_name)
 
@@ -31,13 +34,17 @@ class PDFWriter(WordFolderMgr):
 
     def join_pdf(self):
 
+        # Creo un objeto para unir pdf
         merger = PdfFileMerger()
+        # Le doy todos los que necesita añadir
         for pdf in self.sz_all_pdf:
             merger.append(str(pdf))
 
+        # Le doy la carpeta y el nombre del pdf
         merger.write(str(self.sz_folder / "Reseñas.pdf"))
         merger.close()
 
     def clear_pdf(self):
+        # Elimino los archivos pdf temporales que había escrito
         for file in self.sz_all_pdf:
             os.remove(file)
