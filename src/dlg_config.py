81e67ca2c8f04f130fc6a400783799beba6b8df7
Jorge Luis <jdlatc@yahoo.es>
2021-12-12 11:24:00 +0100

    Create drive section ini

--- src/dlg_config.py
+++ src/dlg_config.py
@@ -21,18 +21,25 @@ class DlgConfig(DlgScrollBase):
     S_READDATA = "READDATA"
     S_COUNT_FILMS = "CONTAR"
     S_POST = "POST"
+    S_DRIVE = "DRIVE"
 
+    # Html
     P_FILTER_PUBLISHED = "Filter_published"
     P_SCRAP_BLOG = "Force_bog_scraping"
+    # Readdata
     P_FILTER_FA = "Filter_FilmAffinity"
     P_DEFAULT_USER = "Mem_user_FA"
+    # Count films
     P_ADD_YEAR = "Add_year"
     P_ADD_INDEX = "Add_index"
     P_WORD_FOLDER = "Folder_with_words"
+    # Post
     P_BLOG_ID = "Blog_id"
     P_DATE = "Posting_date"
     P_TIME = "Posting_time"
     P_AS_DRAFT = "As_draft"
+    # Drive
+    P_FOLDER_ID = "Drive_folder_to_update_id"
 
     def __init__(self):
         super().__init__(question="", options=[], empty_option=True, empty_ans=True)
@@ -63,11 +70,13 @@ class DlgConfig(DlgScrollBase):
         self.add_default_value(self.S_COUNT_FILMS, self.P_ADD_YEAR, False)
         self.add_default_value(self.S_COUNT_FILMS, self.P_ADD_INDEX, False)
         self.add_default_value(self.S_COUNT_FILMS, self.P_WORD_FOLDER, "Word")
-        #Configuraciones para post
+        # Configuraciones para post
         self.add_default_value(self.S_POST, self.P_BLOG_ID, '4259058779347983900')
         self.add_default_value(self.S_POST, self.P_DATE, 'auto')
         self.add_default_value(self.S_POST, self.P_TIME, '20:00')
         self.add_default_value(self.S_POST, self.P_AS_DRAFT, False)
+        # Configuraciones para actualizar drive
+        self.add_default_value(self.S_DRIVE, self.P_FOLDER_ID, '13UbwzbjVFQ8e_UaNalqm_iMihjBDBvtm')
 
     def add_default_value(self, section, param, value):
         # Si no existe la sección, la añado
