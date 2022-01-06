4285c1785781e22c7130fa16d9a75ebb458c92fb
Jorge Luis <jdlatc@yahoo.es>
2022-01-06 13:40:57 +0100

    Use regular expresions in split_title_year

--- src/aux_title_str.py
+++ src/aux_title_str.py
@@ -1,25 +1,14 @@
+import re
+
 def split_title_year(title: str):
     '''
     Dado un título de los escritos en el word,
     quiero extraer el posible año que tenga entre paréntesis
     '''
-    # Busco paréntesis en el título introducido
-    año_primera_pos = title.rfind("(")
-    año_ultima_pos = title.rfind(')')
-
-    # Compruebo si he encontrado paréntesis
-    if año_primera_pos < 0 or año_ultima_pos < 0:
+    # Cadena separada del paréntesis por al menos un espacio
+    # En el paréntesis debe haber 4 dígitos
+    has_year = re.match(r"(.+) +\((\d{4})\)", title)
+    try:
+        return has_year.group(2), has_year.group(1)
+    except AttributeError:
         return "", title
-
-    # Extraigo el posible año
-    año = title[año_primera_pos + 1:año_ultima_pos]
-    # Compruebo que sean 4 dígitos
-    if not año.isnumeric() or len(año) != 4:
-        return "", title
-
-    # En caso contrario efectivamente existe un año
-    title = title[:año_primera_pos]
-    # Quito los espacios que hayan podido quedar
-    title = title.strip()
-
-    return año, title
