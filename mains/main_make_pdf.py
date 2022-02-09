7f7c14dee8191b650b263ac58218b784e5ae9caf
Jorge Luis <jdlatc@yahoo.es>
2022-02-09 11:59:07 +0100

    Call pythoncom

--- mains/main_make_pdf.py
+++ mains/main_make_pdf.py
@@ -6,6 +6,10 @@ from src.dlg_config import manage_config
 def create_PDF(sz_folder):
 
     from src.pdf_writer import PDFWriter
+    import pythoncom
+
+    # Iniclaización necesaria para poder abrir Word con multithreading
+    pythoncom.CoInitialize()
 
     writer = PDFWriter(sz_folder)
     # Convierto cada word a un pdf
@@ -22,11 +26,13 @@ def main(sz_folder):
 
     from src.google_drive import Drive
 
+    # Inicio la conversión a PDF en paralelo
     create_pdf = threading.Thread(name="Create_PDF", target=create_PDF, args=[sz_folder])
     create_pdf.start()
 
     # Actualizo el contenido de google drive
     drive_updater = Drive(sz_folder)
+    # Antes de subir los archivos necesito que el PDF esté terminado
     create_pdf.join()
     drive_updater.update_folder()
 
