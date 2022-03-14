287c71a9073d40086dc4bf41401409a5a6d71c29
Jorge Luis <jdlatc@yahoo.es>
2022-03-14 16:08:19 +0100

    Clear questions

--- src/aux_console.py
+++ src/aux_console.py
@@ -1,4 +1,4 @@
-import os
+import sys
 
 import win32gui as wg
 
@@ -12,14 +12,13 @@ def is_console_on_focus() -> bool:
 
 
 def clear_current_line() -> None:
-    # Me desplazo al inicio de la fila actual
-    # Imprimo tantos espacios como ancho tenga la consola
-    # Evito que se imprima un espacio
-    print("\r" + " " * os.get_terminal_size().columns, end="")
+    # Borro todo el contenido de la linea actual y
+    # me llevo el cursor al inicio de la linea
+    sys.stdout.write('\033[2K\r')
 
 
 def go_to_upper_row() -> None:
-    print("\033[A", end="")
+    sys.stdout.write('\033[A')
 
 
 def delete_line() -> None:
