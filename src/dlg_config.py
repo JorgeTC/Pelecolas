13e6ee2f80a07fe6d91a8dc2df87e62cd115825c
Jorge Luis <jdlatc@yahoo.es>
2022-03-15 17:13:05 +0100

    New param in ini file

--- src/dlg_config.py
+++ src/dlg_config.py
@@ -26,6 +26,7 @@ class DlgConfig(DlgScrollBase):
     P_FILTER_PUBLISHED = "Filter_published"
     P_SCRAP_BLOG = "Force_bog_scraping"
     P_ADD_STYLE = "Write_style"
+    P_YES_ALWAYS_DIR = "New_confidence_director"
     # Readdata
     P_FILTER_FA = "Filter_FilmAffinity"
     P_DEFAULT_USER = "Mem_user_FA"
@@ -64,6 +65,7 @@ class DlgConfig(DlgScrollBase):
         self.add_default_value(self.S_HTML, self.P_FILTER_PUBLISHED, False)
         self.add_default_value(self.S_HTML, self.P_SCRAP_BLOG, False)
         self.add_default_value(self.S_HTML, self.P_ADD_STYLE, False)
+        self.add_default_value(self.S_HTML, self.P_YES_ALWAYS_DIR, "")
         # Configuraciones para readdata
         self.add_default_value(self.S_READDATA, self.P_FILTER_FA, 1)
         self.add_default_value(self.S_READDATA, self.P_DEFAULT_USER, 'Jorge')
