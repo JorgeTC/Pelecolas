f4558e8836a0fc5337cbda8486dd53195fe311ff
Jorge Luis <jdlatc@yahoo.es>
2022-10-15 10:46:02 +0200

    Not clear line in old progressbar

--- src/aux_console.py
+++ src/aux_console.py
@@ -1,5 +1,3 @@
-import sys
-
 import win32gui as wg
 
 # Memorizo la dirección de la consola
@@ -10,8 +8,3 @@ def is_console_on_focus() -> bool:
     # Devuelvo si la ventana en foco actual es la consola
     return wg.GetForegroundWindow() == CURRENT_CONSOLE
 
-def clear_current_line() -> None:
-    # Borro todo el contenido de la linea actual y
-    # me llevo el cursor al inicio de la linea
-    sys.stdout.write('\r\033[2K\r')
-    sys.stdout.flush()
