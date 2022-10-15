e4b99e9e60cb56e1b15a9b73d5084c3a2c3eeded
Jorge Luis <jdlatc@yahoo.es>
2022-10-15 12:47:40 +0200

    Fix DlgUpdatePost returns nothing

--- src/update_blog/dlg_update_post.py
+++ src/update_blog/dlg_update_post.py
@@ -12,16 +12,15 @@ class DlgUpdatePost(DlgScrollBase):
         self.quisiste_decir = TitleMgr(title_list)
 
     def get_ans_body(self) -> str:
-        ans = ""
         # Función sobreescrita de la clase base
-        while not ans:
+        while not self.sz_ans:
             # Inicializo las variables antes de llamar a input
             self.curr_index = -1
             self.sz_options = self.quisiste_decir.get_suggestions()
             self.n_options = len(self.sz_options)
             # Al llamar a input es cuando me espero que se utilicen las flechas
-            ans = input(self.sz_question)
+            self.sz_ans = input(self.sz_question)
             # Se ha introducido un título, compruebo que sea correcto
-            ans = self.quisiste_decir.exact_key(ans)
+            self.sz_ans = self.quisiste_decir.exact_key(self.sz_ans)
 
-        return ans
+        return self.sz_ans
