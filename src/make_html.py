2e4453036b7e2c4494c792a7343bfed45c45fd19
Jorge Luis <jdlatc@yahoo.es>
2022-10-31 23:26:12 +0100

    Extract methods from html

--- src/make_html.py
+++ src/make_html.py
@@ -58,8 +58,6 @@ class html(WordReader):
         else:
             self.data = film
 
-        # Objeto para hacer las citas de forma automática
-        self.__citas: Quoter = None
 
     def ask_for_data(self):
         # Diálogo para pedir los datos necesarios para crear el html
@@ -75,7 +73,7 @@ class html(WordReader):
             self.ask_for_data()
 
         # Preparo el citador con los datos de la película actual
-        self.__citas = Quoter(self.data.titulo, self.data.director)
+        citas = Quoter(self.data.titulo, self.data.director)
 
         # Empiezo a recorrer los párrafos desde el que sé que inicia la crítica que busco
         for paragraph in self.PARAGRAPHS[self.TITULOS[self.data.titulo]:]:
@@ -85,7 +83,7 @@ class html(WordReader):
                 return self.parrafos_critica
 
             # Convierto el contenido del párrafo a html
-            parr_text = self.__parr_to_html(paragraph)
+            parr_text = parr_to_html(paragraph)
 
             if not self.parrafos_critica:
                 # Si es el primer párrafo, elimino el título
@@ -93,47 +91,14 @@ class html(WordReader):
                 parr_text = parr_text.lstrip(": ")
 
             # Añado las citas a otras reseñas
-            parr_text = self.__citas.quote_parr(parr_text)
+            parr_text = citas.quote_parr(parr_text)
 
             # Añado saltos de línea para un html más legible
             parr_text = re.sub(r"([.!?…>:]) ", r"\1\n", parr_text)
             # Añado el texto a la lista de párrafos
             self.parrafos_critica.append(parr_text)
 
-        assert("No ha sido posible encontrar la reseña.")
-
-    def __parr_to_html(self, paragraph: Paragraph) -> str:
-        # Inicializo el párrafo
-        parr_text = ""
-
-        # Conservo las cursivas
-        # Si hay dos cursivas consecutivas las quiero unir como una sola
-        b_prev_it = False
-        b_curr_it = False
-
-        for run in paragraph.runs:
-            # Si todo son espacios, realmente no ha habido cambio de cursivas
-            if not run.text.isspace():
-                b_curr_it = run.italic
-
-            # Acabo de entrar a una cursiva, abro i
-            if b_curr_it and not b_prev_it:
-                parr_text += "<i>"
-            # Acabo de salir de una cursiva, cierro i
-            if not b_curr_it and b_prev_it:
-                parr_text += "</i>"
-
-            # Añado el texto
-            parr_text += run.text
-
-            # Actualizo la itálica del párrafo anterior
-            b_prev_it = b_curr_it
-
-        # Si he terminado el bucle, es posible que me haya dejado la última itálica sin cerrar
-        if b_prev_it:
-            parr_text += "</i>"
-
-        return parr_text
+        assert ("No ha sido posible encontrar la reseña.")
 
     def write_html(self):
 
@@ -145,89 +110,90 @@ class html(WordReader):
         # Compongo el nombre completo del archivo
         self.sz_file_name = SZ_HTML_FILE(self.sz_file_name)
         # Abro el archivo en modo escritura
-        reseña = open(self.html_output_folder / self.sz_file_name,
+        review_file = open(self.html_output_folder / self.sz_file_name,
                       mode="w", encoding="utf-8")
 
         # Escribo el título de la película en mayúsculas.
-        reseña.write(SZ_HTML_TITLE(self.data.titulo.upper()))
+        review_file.write(SZ_HTML_TITLE(self.data.titulo.upper()))
 
         # Escribo el estilo css si así me lo indica el ini
         if Config.get_bool(Section.HTML, Param.ADD_STYLE):
-            reseña.write("<style>\n")
-            reseña.write(
+            review_file.write("<style>\n")
+            review_file.write(
                 open(get_res_folder("Make_html", "template.css")).read())
-            reseña.write("</style>\n")
+            review_file.write("</style>\n")
 
         # Escribo el encabezado
-        reseña.write(SZ_HTML_COMMENT('Encabezado'))
-        reseña.write(SZ_HTML_HEADER(director=self.data.director,
+        review_file.write(SZ_HTML_COMMENT('Encabezado'))
+        review_file.write(SZ_HTML_HEADER(director=self.data.director,
                                     year=self.data.año,
                                     duration=self.data.duracion,
                                     image_url=self.data.url_image))
 
         # Iteramos los párrafos
-        reseña.write(SZ_HTML_COMMENT('Párrafos'))
-        reseña.write('<section class="review-body">\n')
+        review_file.write(SZ_HTML_COMMENT('Párrafos'))
+        review_file.write('<section class="review-body">\n')
         for parrafo in self.parrafos_critica:
             if is_quote_parr(parrafo):
-                reseña.write(SZ_HTML_QUOTE_PARAGRAPH(parrafo))
+                review_file.write(SZ_HTML_QUOTE_PARAGRAPH(parrafo))
             else:
-                reseña.write(SZ_HTML_PARAGRAPH(parrafo))
-        reseña.write('</section>\n')
+                review_file.write(SZ_HTML_PARAGRAPH(parrafo))
+        review_file.write('</section>\n')
 
         # Escribo los botones de Twitter
-        reseña.write(SZ_HTML_BREAK_LINE)
-        reseña.write("\n<footer>")
-        reseña.write(SZ_HTML_COMMENT('Botón follow'))
+        review_file.write(SZ_HTML_BREAK_LINE)
+        review_file.write("\n<footer>")
+        review_file.write(SZ_HTML_COMMENT('Botón follow'))
         html_follow = open(get_res_folder("Make_html", "follow.html")).read()
-        reseña.write(html_follow)
-        reseña.write(SZ_HTML_COMMENT('Botón compartir'))
+        review_file.write(html_follow)
+        review_file.write(SZ_HTML_COMMENT('Botón compartir'))
         html_share = open(get_res_folder("Make_html", "share.html")).read()
-        reseña.write(html_share)
+        review_file.write(html_share)
 
         # Escribo los datos ocultos
-        reseña.write(SZ_HTML_HIDDEN_DATA(year=self.data.año,
+        review_file.write(SZ_HTML_HIDDEN_DATA(year=self.data.año,
                                          director=self.data.director,
                                          country=self.data.pais,
                                          link_fa=self.data.url_FA,
                                          film_title=self.data.titulo,
-                                         labels=self.get_labels(),
+                                         labels=get_labels(self.data),
                                          duration=self.data.duracion,
                                          link_image=self.data.url_image))
-        reseña.write("\n</footer>")
+        review_file.write("\n</footer>")
 
-        reseña.close()
-
-    def get_labels(self) -> str:
-        # Calcula una string con todas las etiquetas estándar que lleva una reseña
-        sz_labels = ""
-        # Cronológicas
-        # Siglo
-        if (int(self.data.año) < 2000):
-            siglo = "Siglo XX"
-        else:
-            siglo = "Siglo XXI"
-        sz_labels += siglo + ", "
-        # Década
-        decade = int(self.data.año) - int(self.data.año) % 10
-        decade = str(decade) + "'s"
-        sz_labels += decade + ", "
-        # Año
-        year = str(self.data.año)
-        sz_labels += year + ", "
-
-        # Director
-        sz_labels += self.data.director + ", "
-
-        # País
-        sz_labels += self.data.pais + ", "
-
-        # Devuelvo la lista de etiquetas
-        return sz_labels
+        review_file.close()
 
     def delete_file(self):
         # Elimino el último html que he escrito
         os.remove(self.html_output_folder / self.sz_file_name)
+
+
+def get_labels(film: Pelicula) -> str:
+    # Calcula una string con todas las etiquetas estándar que lleva una reseña
+    sz_labels = ""
+    # Cronológicas
+    # Siglo
+    if (int(film.año) < 2000):
+        siglo = "Siglo XX"
+    else:
+        siglo = "Siglo XXI"
+    sz_labels += siglo + ", "
+    # Década
+    decade = int(film.año) - int(film.año) % 10
+    decade = str(decade) + "'s"
+    sz_labels += decade + ", "
+    # Año
+    year = str(film.año)
+    sz_labels += year + ", "
+
+    # Director
+    sz_labels += film.director + ", "
+
+    # País
+    sz_labels += film.pais + ", "
+
+    # Devuelvo la lista de etiquetas
+    return sz_labels
 
 
 def is_quote_parr(text: str) -> bool:
@@ -257,6 +223,40 @@ def is_all_italic(text: str) -> bool:
         return False
 
     return True
+
+
+def parr_to_html(paragraph: Paragraph) -> str:
+    # Inicializo el párrafo
+    parr_text = ""
+
+    # Conservo las cursivas
+    # Si hay dos cursivas consecutivas las quiero unir como una sola
+    b_prev_it = False
+    b_curr_it = False
+
+    for run in paragraph.runs:
+        # Si todo son espacios, realmente no ha habido cambio de cursivas
+        if not run.text.isspace():
+            b_curr_it = run.italic
+
+        # Acabo de entrar a una cursiva, abro i
+        if b_curr_it and not b_prev_it:
+            parr_text += "<i>"
+        # Acabo de salir de una cursiva, cierro i
+        if not b_curr_it and b_prev_it:
+            parr_text += "</i>"
+
+        # Añado el texto
+        parr_text += run.text
+
+        # Actualizo la itálica del párrafo anterior
+        b_prev_it = b_curr_it
+
+    # Si he terminado el bucle, es posible que me haya dejado la última itálica sin cerrar
+    if b_prev_it:
+        parr_text += "</i>"
+
+    return parr_text
 
 
 if __name__ == "__main__":
