a242ba3118ab36d1c8c59b4a4cf50708c24c4300
Jorge Luis <jdlatc@yahoo.es>
2022-11-27 18:36:06 +0100

    Use user dlg

--- mains/main_map_directors.py
+++ mains/main_map_directors.py
@@ -2,18 +2,23 @@ import __init__
 from collections import Counter
 from src.excel.read_watched import read_watched
 from src.progress_bar import ProgressBar
+from src.usuario import Usuario
 
 
 def main():
+    usuario = Usuario.ask_user()
+
     # Creo una barra de progreso
     bar = ProgressBar()
 
     directors = Counter()
-    for film, progress in read_watched(1742789):
+    for film, progress in read_watched(usuario.id):
         for director in film.director:
             directors.update({director: 1})
         bar.update(progress)
-    print(directors.most_common())
+    most_common = directors.most_common()
+    for dir, repetitions in most_common:
+        print(f"{dir}: {repetitions}")
     input()
 
 
