84dadfb020a1692131cc2038ae2e6925f2ddcfd2
Jorge Luis <jdlatc@yahoo.es>
2022-12-29 00:25:16 +0100

    New multithreading

---
+++ src/excel/read_watched_imp.py
@@ -0,0 +1,150 @@
+from concurrent.futures import Future, ThreadPoolExecutor
+from math import ceil
+from queue import Queue
+from threading import Thread
+from typing import Iterable
+
+from bs4 import BeautifulSoup
+
+import src.url_FA as url_FA
+from src.config import Config, Param, Section
+from src.excel.film_box import FilmBox
+from src.excel.utils import is_valid, read_film
+from src.pelicula import Pelicula
+from src.safe_url import safe_get_url
+
+
+class ReadWatched:
+    def __init__(self, user_id: int) -> None:
+        self.user_id = user_id
+        self.results: Queue[Pelicula] = Queue()
+        self.total_films = get_total_films(self.user_id)
+        self.box_list = get_all_boxes(self.user_id, self.total_films)
+        self.index = -1
+
+        Thread(target=self.read_watched).start()
+
+    @property
+    def proportion(self) -> float:
+        return (self.index + 1) / self.total_films
+
+    @property
+    def valid_film_list(self) -> Iterable[Pelicula | None]:
+        # Lista de las películas válidas en la página actual.
+        films_list = (self.init_film(box) for box in self.box_list)
+        valid_film_list = (film if is_valid(film) else None
+                           for film in films_list)
+
+        return valid_film_list
+
+    def read_watched(self) -> None:
+        # Iteración convencional de valid_film_list
+        for self.index, film in enumerate(self.valid_film_list):
+            # Si la película no es None, la leo
+            if film is not None:
+                self.results.put(read_film(film))
+
+        # Añado un None para saber cuándo he acabado la iteración
+        self.results.put(None)
+
+    def iter(self) -> Iterable[Pelicula]:
+        while (film := self.results.get()):
+            yield film
+
+    def add_to_queue(self, result: Future):
+        self.index += 1
+        if (film := result.result()):
+            self.results.put(film)
+
+    @staticmethod
+    def init_film(box: FilmBox) -> Pelicula:
+        raise NotImplementedError
+
+
+class ReadDataWatched(ReadWatched):
+    def __init__(self, user_id: int) -> None:
+        ReadWatched.__init__(self, user_id)
+
+    def read_watched(self, *,
+                     use_multithread=Config.get_bool(Section.READDATA, Param.PARALLELIZE)):
+        if use_multithread:
+            self.read_watched_parallel()
+        else:
+            ReadWatched.read_watched()
+
+    def read_watched_parallel(self) -> None:
+        exe = ThreadPoolExecutor(thread_name_prefix="ReadFilm")
+        futures = (exe.submit(read_film, film) if film
+                   else exe.submit(self.noop, film)
+                   for film in self.valid_film_list)
+        for future in futures:
+            future.add_done_callback(self.add_to_queue)
+        exe.shutdown(wait=True)
+        self.results.put(None)
+
+    @staticmethod
+    def init_film(movie_box: FilmBox) -> Pelicula:
+        instance = Pelicula()
+
+        # Guardo los valores que conozco por la información introducida
+        instance.titulo = movie_box.get_title()
+        instance.user_note = movie_box.get_user_note()
+        instance.id = movie_box.get_id()
+        instance.url_FA = url_FA.URL_FILM_ID(instance.id)
+
+        # Devuelvo la instancia
+        return instance
+
+    @staticmethod
+    def noop(_: None) -> None:
+        return None
+
+
+class ReadDirectorsWatched(ReadWatched):
+    def __init__(self, user_id: int) -> None:
+        ReadWatched.__init__(self, user_id)
+
+    @staticmethod
+    def init_film(movie_box: FilmBox) -> Pelicula:
+        instance = Pelicula()
+
+        instance.directors = movie_box.get_directors()
+        instance.titulo = movie_box.get_title()
+
+        return instance
+
+
+def get_total_films(id_user: int) -> int:
+
+    url = url_FA.URL_USER_PAGE(id_user, 1)
+    resp = safe_get_url(url)
+    # Guardo la página ya parseada
+    soup_page = BeautifulSoup(resp.text, 'lxml')
+
+    # me espero que haya un único "value-box active-tab"
+    mydivs = soup_page.find("a", {"class": "value-box active-tab"})
+    stringNumber = str(mydivs.contents[3].contents[1])
+    # Elimino el punto de los millares
+    stringNumber = stringNumber.replace('.', '')
+    return int(stringNumber)
+
+
+def get_all_boxes(user_id: int, total_films: int) -> Iterable[FilmBox]:
+    n_pages = ceil(total_films / 20)
+    url_pages = (url_FA.URL_USER_PAGE(user_id, i + 1)
+                 for i in range(n_pages))
+
+    # Itero todas las páginas del actual usuario
+    for page in url_pages:
+        # Itero la página actual
+        for box in list_boxes(page):
+            yield box
+
+
+def list_boxes(url: str) -> Iterable[FilmBox]:
+    resp = safe_get_url(url)
+    # Guardo la página ya parseada
+    soup_page = BeautifulSoup(resp.text, 'lxml')
+    # Leo todas las películas que haya en ella
+    return (FilmBox(parsed_box) for parsed_box in
+            soup_page.findAll("div", {"class": "user-ratings-movie"}))
--- test/test_readdata.py
+++ test/test_readdata.py
@@ -3,8 +3,9 @@ from unittest import mock
 import pytest
 from bs4 import BeautifulSoup
 
-import src.excel.read_watched as rw
-from src.excel.read_watched import FromFilmBox
+import src.excel.read_watched_imp as rw
+from src.excel.read_watched import read_watched
+from src.excel.film_box import FilmBox
 from src.excel.utils import is_valid
 
 
@@ -19,37 +20,36 @@ def sasha_total_films() -> int:
 
 
 @pytest.fixture
-def sashas_film_boxes(sasha_id, sasha_total_films) -> list[BeautifulSoup]:
-    pages = list(rw.get_all_boxes(sasha_id, sasha_total_films))
-    return sum(pages, [])
+def sashas_film_boxes(sasha_id, sasha_total_films) -> list[FilmBox]:
+    return list(rw.get_all_boxes(sasha_id, sasha_total_films))
 
 
 def test_read_total_films(sasha_id: int, sasha_total_films: int):
     total_films = rw.get_total_films(sasha_id)
     assert total_films == sasha_total_films
-    pages = list(rw.get_all_boxes(sasha_id, total_films))
-    assert len(pages) == sasha_total_films // 20 + 1
-    assert sum(len(page) for page in pages) == sasha_total_films
+    boxes = list(rw.get_all_boxes(sasha_id, total_films))
+    assert len(boxes) == sasha_total_films
 
 
-def test_from_movie_box(sashas_film_boxes: list[BeautifulSoup]):
-    films = (rw.init_film_from_movie_box(box) for box in sashas_film_boxes)
+def test_from_movie_box(sashas_film_boxes: list[FilmBox]):
+    films = (rw.ReadDataWatched.init_film(box) for box in sashas_film_boxes)
     efecto_mariposa = next(film for film in films
                            if film.titulo == 'El efecto mariposa ')
     assert efecto_mariposa.user_note == 2
     assert efecto_mariposa.id == 235464
 
 
-def test_read_data_from_box(sashas_film_boxes: list[BeautifulSoup]):
+def test_read_data_from_box(sashas_film_boxes: list[FilmBox]):
     efecto_mariposa = next(box for box in sashas_film_boxes
-                           if FromFilmBox.get_title(box) == 'El efecto mariposa ')
-    assert FromFilmBox.get_directors(efecto_mariposa) == [
+                           if box.get_title() == 'El efecto mariposa ')
+    assert efecto_mariposa.get_directors() == [
         'Eric Bress', 'J. Mackye Gruber']
-    assert FromFilmBox.get_country(efecto_mariposa) == 'Estados Unidos'
-    assert FromFilmBox.get_year(efecto_mariposa) == 2004
+    assert efecto_mariposa.get_country() == 'Estados Unidos'
+    assert efecto_mariposa.get_year() == 2004
 
 
 @mock.patch.object(is_valid, "__kwdefaults__", {'SET_VALID_FILM': 255})
+@mock.patch.object(rw.ReadDataWatched.read_watched, "__kwdefaults__", {'use_multithread': True})
 def test_readdata(sasha_id: int, sasha_total_films: int):
-    sasha_films = list(rw.read_watched(sasha_id, use_multithread = True))
+    sasha_films = list(read_watched(sasha_id))
     assert len(sasha_films) == sasha_total_films
