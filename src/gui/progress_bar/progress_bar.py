76bab656ba911bfb15cac1b43f2bfd53df1c5874
Jorge Luis <jdlatc@yahoo.es>
2023-01-24 21:13:34 +0100

    Better ProgressBar class

--- src/gui/progress_bar/progress_bar.py
+++ src/gui/progress_bar/progress_bar.py
@@ -1,18 +1,13 @@
-from typing import Optional
-
 from .progress_bar_done import ProgressBarDone
 from .progress_bar_iterations import ProgressBarIterations
+
 
 # Interfaz para barra de progreso.
 # Si introduzco la cantidad de iteraciones avanzará lo correspondiente con cada `update`.
 # De lo contrario hay que introducir la proporción de progreso completado.
 class ProgressBar:
-    def __init__(self, iterations: int = None):
-        self.instance = ProgressBarDone(
-        ) if iterations is None else ProgressBarIterations(iterations)
-
-    def update(self, done: Optional[float] = None):
-        if done is not None:
-            self.instance.update(done)
+    def __new__(self, iterations: int = None):
+        if iterations is not None:
+            return ProgressBarIterations(iterations)
         else:
-            self.instance.update()
+            return ProgressBarDone()
