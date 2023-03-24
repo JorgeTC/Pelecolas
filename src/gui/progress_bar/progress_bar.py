0aad6a03551e7eba931620737a84402d3ff95082
Jorge Luis <jdlatc@yahoo.es>
2023-03-24 20:57:36 +0100

    Inherit ProgressBar

--- src/gui/progress_bar/progress_bar.py
+++ src/gui/progress_bar/progress_bar.py
@@ -1,3 +1,4 @@
+from .progress_bar_base import ProgressBarBase
 from .progress_bar_done import ProgressBarDone
 from .progress_bar_iterations import ProgressBarIterations
 
@@ -5,7 +6,7 @@ from .progress_bar_iterations import ProgressBarIterations
 # Interfaz para barra de progreso.
 # Si introduzco la cantidad de iteraciones avanzará lo correspondiente con cada `update`.
 # De lo contrario hay que introducir la proporción de progreso completado.
-class ProgressBar:
+class ProgressBar(ProgressBarBase):
     def __new__(self, iterations: int = None):
         if iterations is not None:
             return ProgressBarIterations(iterations)
