6e7e9d72beca8451a066b64987559482681bb302
Jorge Luis <jdlatc@yahoo.es>
2023-04-15 14:11:41 +0200

    Refactor searcher

--- test/test_searcher.py
+++ test/test_searcher.py
@@ -19,23 +19,31 @@ def test_unique_film_special_chars():
 
 def test_more_then_one_film_with_same_title():
     searcher = Searcher("La caza")
-    assert searcher.resultados()
+    assert searcher.has_results()
     assert searcher.get_url() == ""
 
 
 def test_more_then_one_with_year():
     searcher = Searcher("La caza (1966)")
-    assert searcher.resultados()
+    assert searcher.has_results()
     assert get_id_from_url(searcher.get_url()) == 399542
+
+    searcher = Searcher("La caza (2012)")
+    assert searcher.has_results()
+    assert get_id_from_url(searcher.get_url()) == 858945
+
+    searcher = Searcher("La caza (2015)")
+    assert searcher.has_results()
+    assert get_id_from_url(searcher.get_url()) == 670127
 
 
 def test_not_found():
     searcher = Searcher("La película que no existe")
-    assert not searcher.resultados()
+    assert not searcher.has_results()
     assert searcher.get_url() == ""
 
 
 def test_two_results_in_same_year():
     searcher = Searcher("Mean girls")
-    assert searcher.resultados()
+    assert searcher.has_results()
     assert searcher.get_url() == ""
