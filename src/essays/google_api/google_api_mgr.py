1f9dd6b370935d91a2ba2e058849754f6929a091
Jorge Luis <jdlatc@yahoo.es>
2023-06-21 23:45:35 +0200

    New get credentials flow

--- src/essays/google_api/google_api_mgr.py
+++ src/essays/google_api/google_api_mgr.py
@@ -1,15 +1,42 @@
-from googleapiclient import sample_tools
-from googleapiclient.discovery import Resource
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import Resource, build
 
 from src.aux_res_directory import get_res_folder
 
 # Dirección del archivo con las credenciales del blog
-SZ_CREDENTIALS = get_res_folder("blog_credentials", "client_secrets.json")
+CREDENTIALS_PATH = get_res_folder("blog_credentials", "client_secrets.json")
+# Dirección del archivo con el token de acceso
+TOKEN_PATH = get_res_folder("blog_credentials", "token.json")
 
 
-def get_google_service(type: str) -> Resource:
-    SERVICE, _ = sample_tools.init(
-        [__file__], type, 'v3', __doc__, SZ_CREDENTIALS,
-        scope=f'https://www.googleapis.com/auth/{type}')
+def get_credentials() -> Credentials:
+    # Defino los servicios de Google que voy a manejar
+    SCOPES = tuple(f"https://www.googleapis.com/auth/{service}"
+                   for service in ('blogger', 'drive'))
 
-    return SERVICE
+    # Si existe el archivo, puedo generar credenciales
+    creds = (Credentials.from_authorized_user_file(TOKEN_PATH, SCOPES)
+             if TOKEN_PATH.is_file() else None)
+
+    # No he obtenido credenciales válidas
+    if not creds or not creds.valid:
+        # Si las puedo refrescar, lo hago
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        # No puedo refrescar, debo generarlo de nuevo
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                CREDENTIALS_PATH, SCOPES)
+            creds = flow.run_local_server(port=0)
+
+        # Actualizo el archivo con el token de acceso
+        with open(TOKEN_PATH, 'w') as token:
+            token.write(creds.to_json())
+
+    return creds
+
+
+def get_google_service(api_type: str) -> Resource:
+    return build(api_type, 'v3', credentials=get_credentials())
