4977dc337073ba63ea337230379d80216d285e3a
JorgeTC <jorgedelatc@gmail.com>
2024-01-13 18:54:02 +0100

    Print each year count

--- mains/main_contar_criticas.py
+++ mains/main_contar_criticas.py
@@ -2,12 +2,35 @@ import __init__
 from src.essays.word import WordReader
 
 
+def print_columns(to_print: list[tuple[str, ...]], separation=1):
+    # Find the maximum width for each column
+    column_widths = [max(len(str(item)) for item in column) + separation
+                     for column in zip(*to_print)]
+
+    # Print the data
+    for row in to_print:
+        for width, column in zip(column_widths, row):
+            print(f"{column:{width}}", end="")
+        print()
+
+
+def print_count_per_year():
+    count_each_year = WordReader.count_each_year()
+
+    # First rows with count for each year
+    years = [(f"{year}:", f"{films_in_year}")
+             for year, films_in_year in count_each_year.items()]
+    all_data = years + [("Total:", f"{len(WordReader.TITULOS)}")]
+
+    print_columns(all_data)
+
+
 def main():
 
     WordReader.list_titles()
     WordReader.write_list()
 
-    print(len(WordReader.TITULOS))
+    print_count_per_year()
     input("Press Enter to continue...")
 
 
--- test/test_word.py
+++ test/test_word.py
@@ -4,25 +4,34 @@ from src.aux_res_directory import get_test_res_folder
 from src.essays.word.word_reader import WordReader
 
 
-def test_read_all_titles():
-    all_titles = ["Película 1",
-                  "Película 2",
-                  "Tan triste como ella (1963)",
-                  "Don de la ebriedad",
-                  "Película 100"]
+all_titles = ["Película 1",
+              "Película 2",
+              "Tan triste como ella (1963)",
+              "Don de la ebriedad",
+              "Película 100"]
 
-    old_name_folder = get_test_res_folder("word", "test_word")
-    with mocks_w.set_word_folder(old_name_folder):
+
+def test_read_all_titles():
+    name_folder = get_test_res_folder("word", "test_word")
+    with mocks_w.set_word_folder(name_folder):
         assert len(WordReader.TITULOS) == len(all_titles)
         for title_key, ref_title in zip(WordReader.TITULOS.keys(), all_titles):
             assert title_key == ref_title
 
 
 def test_ask_year():
-    old_name_folder = get_test_res_folder("word", "test_word")
-    with mocks_w.set_word_folder(old_name_folder):
+    name_folder = get_test_res_folder("word", "test_word")
+    with mocks_w.set_word_folder(name_folder):
         assert WordReader.find_year("Película 1") == 2018
         assert WordReader.find_year("Película 2") == 2018
         assert WordReader.find_year("Tan triste como ella (1963)") == 2018
         assert WordReader.find_year("Don de la ebriedad") == 2019
         assert WordReader.find_year("Película 100") == 2019
+
+
+def test_count_each_year():
+    name_folder = get_test_res_folder("word", "test_word")
+    with mocks_w.set_word_folder(name_folder):
+        count_dict = WordReader.count_each_year()
+        assert count_dict == {2018: 3, 2019: 2}
+        assert sum(count_dict.values()) == len(all_titles)
