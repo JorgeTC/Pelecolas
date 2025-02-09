b92c5c781c28bb405e49fd9b5c0c3ba638a143a4
JorgeTC <jdlatc@yahoo.es>
2025-02-09 01:15:31 +0100

    Refactor QueuedRequest

--- src/essays/google_api/google_client.py
+++ src/essays/google_api/google_client.py
@@ -1,36 +1,32 @@
 from enum import Enum, auto
-from functools import partial
-from multiprocessing import Lock
 from queue import Queue
-from typing import Any, Self
+from threading import Event
+from typing import Any
 
 from googleapiclient.discovery import HttpRequest
 
 
-class QueuedRequest(HttpRequest):
-    def __new__(cls, request: HttpRequest) -> Self:
-        # Modifico el objeto y lo devuelvo
-        cls.__init__(request)
-        return request
+class QueuedRequest:
+    def __init__(self, request: HttpRequest):
+        # La request que ejecutaré
+        self.request = request
 
-    def __init__(self) -> None:
-        # Creo un locker que liberaré cuando tenga la respuesta
-        self.locker = Lock()
-        self.locker.acquire()
+        # Creo una variable en la que guardar el resultado de la request
+        self.result = None
 
-        # Variable para la respuesta de la request
-        self.result: Any = None
+        # Genero un evento que marcaré como set cuando haya guardado la respuesta de la request
+        self.event = Event()
 
-        # Actualizo su método execute
-        self.execute = partial(QueuedRequest.execute, self)
+    def execute(self):
+        # Ejecuto la request, guardo el resultado
+        self.result = self.request.execute()
+        # y marco el evento como completado.
+        self.event.set()
 
-    def execute(self) -> Any:
-        # Ejecuto la request
-        self.result = HttpRequest.execute(self)
-        # Libero el locker para indicar que ya está el resultado
-        self.locker.release()
-
-        # Devuelvo lo que me ha dado el método execute de la clase base
+    def wait_result(self) -> Any:
+        # Espero a que se ejecute el evento y se marque como completado
+        self.event.wait()
+        # Puedo entonces devolver el resultado
         return self.result
 
 
@@ -41,31 +37,28 @@ class RequestsQueue(Enum):
 class GoogleClient:
 
     # Cola de requests por ejecutar
-    REQUESTS_QUEUE: Queue[HttpRequest | None] = Queue()
-
-    # Historial de todas las request
-    requests_ans: dict[str, Any] = {}
+    REQUESTS_QUEUE: Queue[QueuedRequest | RequestsQueue] = Queue()
 
     @classmethod
     def execute(cls, request: HttpRequest) -> None:
         # Añado la request a la cola
-        cls.REQUESTS_QUEUE.put(request)
+        cls.REQUESTS_QUEUE.put(QueuedRequest(request))
 
     @classmethod
     def execute_and_wait(cls, request: HttpRequest) -> Any:
-        # Añado la request a la cola
-        enrich_request = QueuedRequest(request)
-        cls.REQUESTS_QUEUE.put(enrich_request)
+        # Creo un objeto que es capaz de decirme cuándo ha sido ejecutada la request
+        queued_request = QueuedRequest(request)
+        # Le paso la request al ejecutor
+        cls.REQUESTS_QUEUE.put(queued_request)
 
-        # Espero a que se obtenga la respuesta
-        with enrich_request.locker:
-            return enrich_request.result
+        # Pido al objeto que espere hasta que tenga la respuesta de la request
+        return queued_request.wait_result()
 
     @classmethod
     def run_queue(cls):
         while (request := cls.REQUESTS_QUEUE.get()) is not RequestsQueue.DONE:
-            # Ejecuto la request y la guardo en el historial
-            cls.requests_ans[request.uri] = request.execute()
+            # Ejecuto la request
+            request.execute()
             # Indico que he acabado con la última request sacada de la cola
             cls.REQUESTS_QUEUE.task_done()
 
