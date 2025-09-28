da64432c408615149e2bb0da2f2faf6719375261
JorgeTC <jdlatc@yahoo.es>
2025-09-28 17:31:25 +0200

    First log messages

--- src/safe_url/safe_url.py
+++ src/safe_url/safe_url.py
@@ -1,4 +1,5 @@
 from http import HTTPStatus
+import logging
 
 from requests.models import Response
 
@@ -7,10 +8,12 @@ from .pass_captcha import PassCaptcha
 
 
 def safe_get_url(url: str) -> Response:
+    logging.debug(f"Requesting url: {url}")
     # open with GET method
     resp = safe_response(url)
     # Caso 429: too many requests
     if resp.status_code == HTTPStatus.TOO_MANY_REQUESTS:
+        logging.debug(f"Too many requests for url: {url}")
         return PassCaptcha(url)
     else:
         return resp
